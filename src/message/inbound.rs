417544472736c34ec2d181605d62c1be2fc1bc00
asimsedhain <asimsedhain33@gmail.com>
2023-09-30 14:40:23 -0500

    feat: remove lifetime params from inbound message

--- src/message/inbound.rs
+++ src/message/inbound.rs
@@ -4,19 +4,17 @@ use enum_primitive::*;
 use solace_rs_sys as ffi;
 use std::convert::From;
 use std::ffi::CStr;
-use std::marker::PhantomData;
 use std::ptr;
 use std::time::{Duration, SystemTime};
 use tracing::warn;
 
-pub struct InboundMessage<'a> {
+pub struct InboundMessage {
     _msg_ptr: ffi::solClient_opaqueMsg_pt,
-    _phantom: PhantomData<&'a u8>,
 }
 
-unsafe impl Send for InboundMessage<'_> {}
+unsafe impl Send for InboundMessage {}
 
-impl Drop for InboundMessage<'_> {
+impl Drop for InboundMessage {
     fn drop(&mut self) {
         let msg_free_result = unsafe { ffi::solClient_msg_free(&mut self._msg_ptr) };
         if SolClientReturnCode::from_i32(msg_free_result) != Some(SolClientReturnCode::Ok) {
@@ -25,7 +23,7 @@ impl Drop for InboundMessage<'_> {
     }
 }
 
-impl From<ffi::solClient_opaqueMsg_pt> for InboundMessage<'_> {
+impl From<ffi::solClient_opaqueMsg_pt> for InboundMessage {
     /// .
     ///
     /// # Safety
@@ -36,20 +34,17 @@ impl From<ffi::solClient_opaqueMsg_pt> for InboundMessage<'_> {
     ///
     /// .
     fn from(ptr: ffi::solClient_opaqueMsg_pt) -> Self {
-        Self {
-            _msg_ptr: ptr,
-            _phantom: PhantomData,
-        }
+        Self { _msg_ptr: ptr }
     }
 }
 
-impl<'a> Message<'a> for InboundMessage<'a> {
+impl<'a> Message<'a> for InboundMessage {
     unsafe fn get_raw_message_ptr(&self) -> ffi::solClient_opaqueMsg_pt {
         self._msg_ptr
     }
 }
 
-impl<'a> InboundMessage<'a> {
+impl InboundMessage {
     pub fn get_receive_timestamp(&self) -> Result<Option<SystemTime>> {
         let mut ts: i64 = 0;
         let op_result =
@@ -64,7 +59,7 @@ impl<'a> InboundMessage<'a> {
         }
     }
 
-    pub fn get_sender_id(&'a self) -> Result<Option<&'a str>> {
+    pub fn get_sender_id(&self) -> Result<Option<&str>> {
         let mut buffer = ptr::null();
 
         let msg_ops_result =
