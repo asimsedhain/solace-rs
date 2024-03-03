63011d7d70edefd2bd1b1045f65e2160443c521e
asimsedhain <asimsedhain33@gmail.com>
2024-03-03 12:36:44 -0600

    refactor: use session builder for session method

--- src/context.rs
+++ src/context.rs
@@ -1,17 +1,14 @@
 use crate::session::builder::SessionBuilder;
+use crate::session::builder::SessionBuilderError;
 use crate::Session;
-use crate::SessionError;
 use crate::{ContextError, SolClientReturnCode, SolaceLogLevel};
 use solace_rs_sys as ffi;
-use std::marker::PhantomData;
 use std::mem;
 use std::ptr;
 use tracing::warn;
 
 use crate::event::SessionEvent;
 use crate::message::InboundMessage;
-use crate::util::{on_event_trampoline, on_message_trampoline};
-use std::ffi::CString;
 use std::sync::Arc;
 type Result<T> = std::result::Result<T, ContextError>;
 
@@ -122,118 +119,37 @@ impl Context {
         SessionBuilder::new(self.clone())
     }
 
-    pub fn session<'session, H, V, U, P, M, E>(
+    pub fn session<'session, Host, Vpn, Username, Password, OnMessage, OnEvent>(
         &self,
-        host_name: H,
-        vpn_name: V,
-        username: U,
-        password: P,
-        on_message: Option<M>,
-        on_event: Option<E>,
-    ) -> std::result::Result<Session<'session>, SessionError>
+        host_name: Host,
+        vpn_name: Vpn,
+        username: Username,
+        password: Password,
+        on_message: Option<OnMessage>,
+        on_event: Option<OnEvent>,
+    ) -> std::result::Result<Session<'session>, SessionBuilderError>
     where
-        H: Into<Vec<u8>>,
-        V: Into<Vec<u8>>,
-        U: Into<Vec<u8>>,
-        P: Into<Vec<u8>>,
-        M: FnMut(InboundMessage) + Send + 'session,
-        E: FnMut(SessionEvent) + Send + 'session,
+        Host: Into<Vec<u8>>,
+        Vpn: Into<Vec<u8>>,
+        Username: Into<Vec<u8>>,
+        Password: Into<Vec<u8>>,
+        OnMessage: FnMut(InboundMessage) + Send + 'session,
+        OnEvent: FnMut(SessionEvent) + Send + 'session,
     {
-        /* Session */
-        //solClient_opaqueSession_pt session_p;
-        //solClient_session_createFuncInfo_t sessionFuncInfo = SOLCLIENT_SESSION_CREATEFUNC_INITIALIZER;
+        let mut builder = SessionBuilder::new(self.clone())
+            .host_name(host_name)
+            .vpn_name(vpn_name)
+            .username(username)
+            .password(password);
 
-        // Converting props and storing them session props
-        let c_host_name = CString::new(host_name)?;
-        let c_vpn_name = CString::new(vpn_name)?;
-        let c_username = CString::new(username)?;
-        let c_password = CString::new(password)?;
-
-        // Session props is a **char in C
-        // it takes in an array of key and values
-        // first we specify the key, then the value
-        // Session also copies over the props and maintains a copy internally.
-        // Note: Needs to live long enough for the values to be copied
-        let mut session_props = [
-            ffi::SOLCLIENT_SESSION_PROP_HOST.as_ptr() as *const i8,
-            c_host_name.as_ptr(),
-            ffi::SOLCLIENT_SESSION_PROP_VPN_NAME.as_ptr() as *const i8,
-            c_vpn_name.as_ptr(),
-            ffi::SOLCLIENT_SESSION_PROP_USERNAME.as_ptr() as *const i8,
-            c_username.as_ptr(),
-            ffi::SOLCLIENT_SESSION_PROP_PASSWORD.as_ptr() as *const i8,
-            c_password.as_ptr(),
-            ffi::SOLCLIENT_SESSION_PROP_CONNECT_BLOCKING.as_ptr() as *const i8,
-            ffi::SOLCLIENT_PROP_ENABLE_VAL.as_ptr() as *const i8,
-            ptr::null(),
-        ];
-
-        let mut session_pt: ffi::solClient_opaqueSession_pt = ptr::null_mut();
-
-        // Box::into_raw(Box::new(Box::new(f))) as *mut _
-        // leaks memory
-        // but without it, causes seg fault
-        let (static_on_message_callback, user_on_message) = match on_message {
-            Some(f) => (
-                on_message_trampoline(&f),
-                Box::into_raw(Box::new(Box::new(f))) as *mut _,
-            ),
-            _ => (None, ptr::null_mut()),
-        };
-
-        let (static_on_event_callback, user_on_event) = match on_event {
-            Some(f) => (
-                on_event_trampoline(&f),
-                Box::into_raw(Box::new(Box::new(f))) as *mut _,
-            ),
-            _ => (None, ptr::null_mut()),
-        };
-
-        // Function information for Session creation.
-        // The application must set the eventInfo callback information. All Sessions must have an event callback registered.
-        let mut session_func_info: ffi::solClient_session_createFuncInfo_t =
-            ffi::solClient_session_createFuncInfo {
-                rxInfo: ffi::solClient_session_createRxCallbackFuncInfo {
-                    callback_p: ptr::null_mut(),
-                    user_p: ptr::null_mut(),
-                },
-                eventInfo: ffi::solClient_session_createEventCallbackFuncInfo {
-                    callback_p: static_on_event_callback,
-                    user_p: user_on_event,
-                },
-                rxMsgInfo: ffi::solClient_session_createRxMsgCallbackFuncInfo {
-                    callback_p: static_on_message_callback,
-                    user_p: user_on_message,
-                },
-            };
-
-        let session_create_raw_rc = unsafe {
-            ffi::solClient_session_create(
-                session_props.as_mut_ptr(),
-                self.raw.ctx,
-                &mut session_pt,
-                &mut session_func_info,
-                std::mem::size_of::<ffi::solClient_session_createFuncInfo_t>(),
-            )
-        };
-
-        let rc = SolClientReturnCode::from_raw(session_create_raw_rc);
-
-        if !rc.is_ok() {
-            return Err(SessionError::InitializationFailure(rc));
+        if let Some(on_message) = on_message {
+            builder = builder.on_message(on_message);
         }
 
-        let connection_raw_rc = unsafe { ffi::solClient_session_connect(session_pt) };
-
-        let rc = SolClientReturnCode::from_raw(connection_raw_rc);
-        if rc.is_ok() {
-            Ok(Session {
-                _session_pt: session_pt,
-                context: self.clone(),
-                lifetime: PhantomData,
-            })
-        } else {
-            Err(SessionError::ConnectionFailure(rc))
+        if let Some(on_event) = on_event {
+            builder = builder.on_event(on_event);
         }
+
+        builder.build()
     }
 }
